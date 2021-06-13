72f5833138a7b3f231ee897c627861ab8d9de697
isiemaszko <izabela.siemaszko@onet.pl>
2021-06-13 10:59:17 +0200

    views

--- app/views/server.js
+++ app/views/server.js
@@ -10,8 +10,10 @@ dotenv.config({path:'config.env'})
 app.use(morgan('tiny'))
 app.use(bodyparser.urlencoded({extended:true}))
 app.set("view engine","ejs")
+app.set('views', path.join(__dirname, 'views'));
 app.get('/',(req,res)=>{
-    res.render("index");
+  //  res.render("index");
+    res.send("hello world")
 })
 
 app.listen(8080,()=>{console.log('Server i running on http://localhost:8080')})
